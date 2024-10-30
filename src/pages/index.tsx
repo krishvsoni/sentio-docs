99208b4b3045181a3cf93fdedc2b668d46a42903
haard18 <haard-cse-student@itmbu.ac.in>
2024-10-30 23:04:12 +0530

    Update homepage title and description for improved clarity and focus

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,8 +47,8 @@ export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title} Sentio Docs`}
+      description="Walkthrough towards Security">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
